0e17f91fe2fc43236cd3d6ca7f4e78eee1bfbda1
Marcos Pozzo <pozzomarc@gmail.com>
2021-04-23 17:58:46 -0300

    input fields added to work directly on the browser

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   $("#png").click(function () {
-    domtoimage.toBlob(document.getElementById("div")).then(function (blob) {
+    domtoimage.toBlob(document.getElementById("canvas")).then(function (blob) {
       window.saveAs(blob, $("#name").text() + ".png");
     });
   });
   $("#jpg").click(function () {
     domtoimage
-      .toJpeg(document.getElementById("div"), { quality: 0.95 })
+      .toJpeg(document.getElementById("canvas"), { quality: 0.95 })
       .then(function (dataUrl) {
         var link = document.createElement("a");
         link.download = $("#name").text() + ".jpg";
@@ -14,4 +14,21 @@ $(document).ready(function () {
         link.click();
       });
   });
+});
+
+$("#input-name").on("input", function () {
+  $("#name").text($("#input-name").val());
+});
+
+$("#input-country").on("input", function () {
+  $("#country").text($("#input-country").val());
+});
+
+$("#input-picture").on("change", function () {
+  $("#image").attr(
+    "src",
+    $("#input-picture")
+      .val()
+      .match(/[^\\/]*$/)[0] // this regex matches the file name because of the c fakepath problem
+  );
 });
